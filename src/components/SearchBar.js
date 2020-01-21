adcd11b109d4b05959c0f66ecd6f2cb1de56193f
Alec Budd <alec.budd@prodyna.com>
2020-01-21 13:01:56 +0100

    search bar developing

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
-import {View, TextInput, StyleSheet} from 'react-native';
+import React from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 
-const SearchBar = () => {
-    return (
-        <View style={styles.backgroundStyle}>
-            <Feather style={styles.iconStyle} name="search" size={30}/>
-            <TextInput style={styles.inputStyle} placeholder="Search"/>
-        </View>
-    )
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+   return (
+      <View style={styles.backgroundStyle}>
+         <Feather style={styles.iconStyle} name="search" size={30}/>
+         <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.inputStyle}
+            placeholder="Search"
+            value={term}
+            onChangeText={newTerm => onTermChange(newTerm)}
+            onEndEditing={() => onTermSubmit()}
+         />
+      </View>
+   )
 };
 
 const styles = StyleSheet.create({
-    backgroundStyle: {
-        marginTop: 10,
-        backgroundColor: '#F0EEEE',
-        height: 50,
-        borderRadius: 5,
-        marginHorizontal: 15,
-        flexDirection: 'row'
-    },
-    inputStyle: {
-        flex: 1,
-        fontSize: 18
-    },
-    iconStyle: {
-        fontSize: 35,
-        alignSelf: 'center',
-        marginHorizontal: 15
-    }
+   backgroundStyle: {
+      marginTop: 10,
+      backgroundColor: '#F0EEEE',
+      height: 50,
+      borderRadius: 5,
+      marginHorizontal: 15,
+      flexDirection: 'row'
+   },
+   inputStyle: {
+      flex: 1,
+      fontSize: 18
+   },
+   iconStyle: {
+      fontSize: 35,
+      alignSelf: 'center',
+      marginHorizontal: 15
+   }
 });
 
 export default SearchBar;
